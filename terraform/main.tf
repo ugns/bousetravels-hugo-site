0f8e12d39ea0a57421ae80dd2eb999feff707634
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-06-06 10:44:06 -0400

    Add GitHub Action workflow for Amplify deployment

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+resource "aws_amplify_app" "website" {
+  name       = var.domain_name
+  repository = var.repository
+
+  platform                    = "WEB"
+  enable_auto_branch_creation = true
+  enable_branch_auto_deletion = true
+  auto_branch_creation_patterns = [
+    "*",
+    "*/**",
+  ]
+
+  custom_rule {
+    source = "/.well-known/<*>"
+    status = "200"
+    target = "/well-known/<*>.txt"
+  }
+
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/404.html"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id                      = aws_amplify_app.website.id
+  branch_name                 = "main"
+  stage                       = "PRODUCTION"
+  enable_pull_request_preview = false
+  framework                   = "Web"
+}
+
+resource "aws_amplify_branch" "beta" {
+  app_id                      = aws_amplify_app.website.id
+  branch_name                 = "develop"
+  stage                       = "BETA"
+  enable_pull_request_preview = true
+}
+
+resource "aws_amplify_domain_association" "website" {
+  app_id                 = aws_amplify_app.website.id
+  domain_name            = var.domain_name
+  enable_auto_sub_domain = true
+
+  certificate_settings {
+    custom_certificate_arn = data.aws_acm_certificate.this.arn
+    type                   = "CUSTOM"
+  }
+
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = ""
+  }
+
+  sub_domain {
+    branch_name = aws_amplify_branch.beta.branch_name
+    prefix      = "beta"
+  }
+
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,13 @@
+namespace = "bta"
+
+environment = "use1"
+
+stage = "prod"
+
+name = "website"
+
+repository = "https://github.com/ugns/bousetravels-hugo-site"
+
+domain_name = "bousetravels.world"
+
+comment = "Bouse Travel Advisors"
