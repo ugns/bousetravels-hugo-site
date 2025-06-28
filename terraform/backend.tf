1303dbe8a306825183dcaaee04ba2a9f6cb752e9
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-06-28 11:17:24 -0400

    fix: remove unnecessary name from terraform job and clean up backend.tf configuration
    Removes unnecessary name from terraform job and cleans backend
    
    Eliminates the redundant job name from the Terraform workflow for clarity.
    
    Cleans up the backend configuration by removing an empty profile entry, streamlining the setup for better readability and maintenance.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,7 +5,6 @@ terraform {
     region         = "us-east-1"
     bucket         = "ugns-use1-terraform-state"
     dynamodb_table = "ugns-use1-terraform-state-lock"
-    profile        = ""
     encrypt        = "true"
     assume_role = {
       role_arn = "arn:aws:iam::465691465286:role/GitHubActionsExecution"
