a1fd077dcd07190fbe4d6dd0ff22104a0f6e7c4f
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-06-30 13:53:56 -0400

    feat: add ACM certificate module configuration in Terraform
    Adds ACM certificate module configuration in Terraform
    
    Implements a new module for requesting ACM certificates, enabling domain validation and allowing for wildcard alternative names. This setup improves SSL management for the associated domain.

---
+++ terraform/acm.tf
@@ -0,0 +1,10 @@
+module "acm_certificate" {
+  source  = "cloudposse/acm-request-certificate/aws"
+  version = "0.18.0"
+
+  domain_name                       = aws_route53_zone.this.name
+  zone_id                           = aws_route53_zone.this.id
+  process_domain_validation_options = true
+  ttl                               = "300"
+  subject_alternative_names         = [join(".", ["*", aws_route53_zone.this.name])]
+}
